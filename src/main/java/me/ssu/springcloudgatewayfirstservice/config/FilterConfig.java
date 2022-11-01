b68c46c1ecdf512701dca2a79e0b4bf2f36c3054
김수환 <zzanggoon8@hiconsy.com>
2022-11-01 19:04:25 +0900

    filter config Add

--- src/main/java/me/ssu/springcloudgatewayfirstservice/config/FilterConfig.java
+++ src/main/java/me/ssu/springcloudgatewayfirstservice/config/FilterConfig.java
@@ -12,8 +12,7 @@ public class FilterConfig {
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/first-service/**")
-						.filters(f ->
-								f.addRequestHeader("first-request", "first-request-header")
+						.filters(f -> f.addRequestHeader("first-request", "first-request-header")
 								.addResponseHeader("first-response", "first-response-header"))
 						.uri("http://localhost:8081/"))
 				.build();
