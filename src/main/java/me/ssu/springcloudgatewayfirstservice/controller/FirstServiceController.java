9c243c577a256a2d8a0fcab36ef54a29658f209e
김수환 <zzanggoon8@hiconsy.com>
2022-11-01 18:38:36 +0900

    FirstServiceController Add

--- src/main/java/me/ssu/springcloudgatewayfirstservice/controller/FirstServiceController.java
+++ src/main/java/me/ssu/springcloudgatewayfirstservice/controller/FirstServiceController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// http://localhost:8081/welcome
+// http://localhost:8081/first-service/welcome
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
